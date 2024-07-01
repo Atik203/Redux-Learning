9736b4733dff30b938f0a15d55213071a5054642
Atikur Rahaman <atikurrahaman0305@gmail.com>
2024-07-01 21:31:32 +0600

    init redux store config

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,13 @@
+import Counter from "./components/layouts/Counter";
+
 const App = () => {
   return (
-    <div className="text-center font-bold text-4xl my-10">
-      <h1>React App</h1>
-      <p>React App with TypeScript</p>
+    <div>
+      <div className="text-center font-bold text-2xl my-10">
+        <h1>React Redux</h1>
+        <p>React Redux Learning with TypeScript</p>
+      </div>
+      <Counter />
     </div>
   );
 };
