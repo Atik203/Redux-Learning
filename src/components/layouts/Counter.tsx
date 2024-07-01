0e93928a430512445436bad1271ceaf32535066a
Atikur Rahaman <atikurrahaman0305@gmail.com>
2024-07-01 21:45:46 +0600

    added simple counterSlice

--- src/components/layouts/Counter.tsx
+++ src/components/layouts/Counter.tsx
@@ -1,20 +1,27 @@
-import { useState } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import {
+  decrement,
+  increment,
+} from "../../redux/features/counter/counterSlice";
+import { RootState } from "../../redux/store";
 
 const Counter = () => {
-  const [count, setCount] = useState(0);
+  const count = useSelector((state: RootState) => state.counter.count);
+
+  const dispatch = useDispatch();
 
   return (
     <div className="flex items-center justify-center space-x-4">
       <button
         className="px-4 py-2 bg-red-500 text-white rounded hover:bg-red-700"
-        onClick={() => setCount(count - 1)}
+        onClick={() => dispatch(decrement())}
       >
         Decrement
       </button>
       <span className="text-xl">{count}</span>
       <button
         className="px-4 py-2 bg-blue-500 text-white rounded hover:bg-blue-700"
-        onClick={() => setCount(count + 1)}
+        onClick={() => dispatch(increment())}
       >
         Increment
       </button>
