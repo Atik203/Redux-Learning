b6b31cd14cf26576cd6ad3f4e34cf2d06ed63cad
Atikur Rahaman <atikurrahaman0305@gmail.com>
2024-07-01 21:21:22 +0600

    setup the project

---
+++ src/App.tsx
@@ -0,0 +1,10 @@
+const App = () => {
+  return (
+    <div className="text-center font-bold text-4xl my-10">
+      <h1>React App</h1>
+      <p>React App with TypeScript</p>
+    </div>
+  );
+};
+
+export default App;
